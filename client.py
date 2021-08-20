d1a3125b608df97dc5ec647f0862326d87999dd1
gtambara <gabr.tabajara@gmail.com>
2021-08-20 16:45:12 -0300

    Created GUI with Tkinter

--- client.py
+++ client.py
@@ -1,36 +1,49 @@
 from socket import *
+import tkinter as tk
 
 serverName = 'localhost'
 serverPort = 12000
 
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 
-dados = input('Coloque sua frase em letras minúsculas:')
-cor = input('Coloque a sua cor favorita:[1-azul,2-vermelho,3-verde,4-amarelo,5-branco]')
+def botaoPressionado(event):
+    dados = entry.get()
+    mensagem = dados
+    
+    # Envio da mensagem em bytes pro servidor
+    clientSocket.sendto(mensagem.encode(),(serverName, serverPort))
 
-switchCase = {
-        1: '\033[34m',
-        2: '\033[31m',
-        3: '\033[32m',
-        4: '\033[33m',
-        5: '\033[37m'
-    }
+    # Recebimento da mensagem modificada do servidor
+    mensagemNova, serverAddress = clientSocket.recvfrom(2048)
 
-print(switchCase[int(cor)])
+    # Mostra a mensagem modificada como string
+    try:
+        resposta.tk.destroy()
+    except (NameError, AttributeError):
+        pass
+    resposta.config(text="Retorno do servidor:\n" + mensagemNova.decode(), bg="green")
+    resposta.pack()
 
-# mensagem = switchCase[int(cor)] + " " + dados + " " + '\033[37m'
-mensagem = dados
+window = tk.Tk()
+window.geometry("300x220")
+label = tk.Label(text="Frase mandada ao servidor:")
+entry = tk.Entry()
 
-# Envio da imagem em bytes pro servidor
-clientSocket.sendto(mensagem.encode(),(serverName, serverPort))
+respostaFrame = tk.Frame(window, height = "80", width = "110", bg = "green")
+respostaFrame.pack(fill = 'both')
+resposta = tk.Label(respostaFrame, text="", fg="white", bg="green", height = "5", wraplength = 160)
+resposta.pack()
 
-# Recebimento da mensagem modificada do servidor
-mensagemNova, serverAddress = clientSocket.recvfrom(2048)
+b1 = tk.Button(window, text="click", bg="red", fg="white")
 
-# Mostra a mensagem modificada como string
-print(mensagemNova.decode())
+entry.pack()
+b1.pack()
+label.pack()
+
+b1.bind('<Button>', botaoPressionado)
+
+window.mainloop()
 
 # Fecha o socket/conexão com o servidor
 clientSocket.close()
-
 
